9b5ce99da60f5f86e2c695228c3600009404a32a
Vyacheslav Nekrasov <slnec@yandex.ru>
2022-12-16 00:34:26 +0700

    Initial commit

---
+++ FileManagerSample/FileManagerSample/FileManagerViewController.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+class FileManagerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
