b24e0efe4a7bf89758a69d13e4d2f2dc2bb063a5
Gameover1987 <slnec@yandex.ru>
2023-02-02 00:15:36 +0700

    Выпилил кучу всего лишнего

---
+++ MapNavigationSample/MapNavigationSample/MapNavigationViewController.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+final class MapNavigationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
